9d8ccceb5cf2ed3281e7fcf0ebbf5636d157510d
tylerg05 <tylergardella05@gmail.com>
2021-04-05 20:43:38 -0500

    Add project files.

---
+++ Lesson3LabB/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lesson3LabB
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Btn_Clicked(Object sender, System.EventArgs e)
+        {
+            int yob = Date_Age.Date.Year;
+            int age = 2021 - yob;
+            if (age >= 21)
+            {
+                Label_Message.Text = "You are already drinking age.";
+            }
+            else
+            {
+                int yearsUntilDrink = 21 - age;
+                Label_Message.Text = $"You have {yearsUntilDrink} years until you can legally buy alcohol";
+            }
+        }
+    }
+}
